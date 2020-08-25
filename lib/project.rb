5623979685fce18f3de8aafec9ab1764a7b8cdd7
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-25 20:19:56 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,10 @@
 class Project
 
+  attr_reader :title
+  attr_accessor :backed_projects
+
+  def initialize(name, backed_projects = [])
+    @name = name
+    @backed_projects = backed_projects
+  end
 end
