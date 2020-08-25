df60ddeb7b46dde54f4afb71b77257553f14e586
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-25 20:15:16 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,7 +3,7 @@ class Backer
   attr_reader :name
   attr_accessor :backed_projects
 
-  def initialize(name, backed_projects)
+  def initialize(name, backed_projects = [])
     @name = name
     @backed_projects = backed_projects
   end
